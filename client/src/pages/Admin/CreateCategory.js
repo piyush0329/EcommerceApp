641bdd56f86af43821020e00d5388db9cdb417c2
piyush0329 <upadhyaypiyush29@gmail.com>
2023-08-29 02:36:19 +0530

    Product API and Category API with CRUD operations

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/Layout/Layout'
 import AdminMenu from '../../components/Layout/AdminMenu'
+import toast from "react-hot-toast";
+import axios from 'axios';
 
 const CreateCategory = () => {
+  const [categories, setCategories] = useState([])
+
+  //get all categories
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get('http://localhost:8000/api/v1/category/get-category')
+      if (data.success) {
+        setCategories(data.category)
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went Wrong in getting category')
+    }
+  }
+
+  useEffect(() => {
+    getAllCategory()
+  }, [])
   return (
     <Layout title={"Dashboard - Create Category"}>
       <div className="container-fluid m-3 p-3">
@@ -11,7 +31,46 @@ const CreateCategory = () => {
             <AdminMenu />
           </div>
           <div className="col-md-9">
-            <h1>Create Category</h1>
+            <h1>Manage Category</h1>
+            <div className="w-75">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {categories?.map((c) => (
+                    <>
+                      <tr >
+                        <td key={c._id}>{c.name}</td>
+                        <td>
+                          <button
+                            className="btn btn-primary ms-2"
+                            onClick={() => {
+                              setVisible(true);
+                              setUpdatedName(c.name);
+                              setSelected(c);
+                            }}
+                          >
+                            Edit
+                          </button>
+                          <button
+                            className="btn btn-danger ms-2"
+                            onClick={() => {
+                              handleDelete(c._id);
+                            }}
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    </>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
