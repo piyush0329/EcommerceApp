988f2787c9f42e1c5ca0b9a6f630ac6066868587
piyush0329 <upadhyaypiyush29@gmail.com>
2023-09-20 01:57:10 +0530

    for development

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import authRoutes from './routes/authRoute.js'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors'
+
+import path from 'path'
 //config env
 dotenv.config()
 
@@ -20,17 +22,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
-
+app.use(express.static(path.join(__dirname,'./client/build')))
 //routes
 
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/category', categoryRoutes)
 app.use('/api/v1/product',productRoutes)
 
+
 //rest api
-app.get('/', (req, res) => {
-    res.send("<h1>Welcome to front page</h1>")
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
+
 
 //PORT
 const PORT = process.env.PORT || 8000
